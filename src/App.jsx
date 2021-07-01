b70c39933942ee6c5483251137872d6a7caeb7a4
Greg-One <sgmdoc@yandex.ru>
2021-07-01 07:48:36 +0300

    add drawer list styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ export function App() {
     <div className="wrapper">
       <div className="drawer-overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <div className="drawer-header">
+            <h2>Корзина</h2>
+            <button>
+              <img src="/img/cart-remove.svg" alt="Remove" />
+            </button>
+          </div>
           <div className="drawer-items">
             <div className="cart-item">
               <img
@@ -19,18 +24,7 @@ export function App() {
                 <b>12 999 руб.</b>
               </div>
               <button>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.0799 10.6155L9.6311 8.16673L12.0798 5.71801C13.0241 4.77376 11.5596 3.30934 10.6154 4.25359L8.16668 6.70231L5.71787 4.2535C4.77384 3.30947 3.30947 4.77384 4.2535 5.71787L6.70231 8.16668L4.25359 10.6154C3.30934 11.5596 4.77376 13.0241 5.71801 12.0798L8.16673 9.6311L10.6155 12.0799C11.5597 13.0241 13.0241 11.5597 12.0799 10.6155Z"
-                    fill="#D3D3D3"
-                  />
-                </svg>
+                <img src="/img/cart-remove.svg" alt="Remove" />
               </button>
             </div>
 
@@ -46,33 +40,43 @@ export function App() {
                 <b>12 999 руб.</b>
               </div>
               <button>
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12.0799 10.6155L9.6311 8.16673L12.0798 5.71801C13.0241 4.77376 11.5596 3.30934 10.6154 4.25359L8.16668 6.70231L5.71787 4.2535C4.77384 3.30947 3.30947 4.77384 4.2535 5.71787L6.70231 8.16668L4.25359 10.6154C3.30934 11.5596 4.77376 13.0241 5.71801 12.0798L8.16673 9.6311L10.6155 12.0799C11.5597 13.0241 13.0241 11.5597 12.0799 10.6155Z"
-                    fill="#D3D3D3"
-                  />
-                </svg>
+                <img src="/img/cart-remove.svg" alt="Remove" />
+              </button>
+            </div>
+
+            <div className="cart-item">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="cart-info">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <button>
+                <img src="/img/cart-remove.svg" alt="Remove" />
               </button>
             </div>
           </div>
-          <ul className="drawer-list">
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 руб.</b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
+          <div className="drawer-block">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб.</b>
+              </li>
+            </ul>
+            <button className="button-green">
+              Оформить заказ <img src="/img/cart-arrow.svg" alt="Arrow" />
+            </button>
+          </div>
         </div>
       </div>
 
