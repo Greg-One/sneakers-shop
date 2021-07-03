d5a70e770a3c51be702b3179d62768719e1252d1
Greg-One <sgmdoc@yandex.ru>
2021-07-03 22:52:02 +0300

    add plus button states

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import styles from './Card.module.scss';
 
 export function Card(props) {
+  const [isAdded, setIsAdded] = React.useState(false);
+
+  const handlePlusClick = () => {
+    setIsAdded(!isAdded);
+  };
+
   return (
     <article className={styles.card}>
-      <button className={styles.like}>
+      <button className={styles.like} onClick={props.onClickLike}>
         <img src="/img/like-disabled.svg" alt="Like" />
       </button>
 
@@ -15,8 +21,14 @@ export function Card(props) {
           <span>Цена:</span>
           <b>{props.price} руб.</b>
         </div>
-        <button>
-          <img src="/img/plus.svg" alt="Add item" />
+        <button
+          onClick={handlePlusClick}
+          className={isAdded ? styles.plus : ''}
+        >
+          <img
+            src={isAdded ? '/img/plus-checked.svg' : '/img/plus.svg'}
+            alt="Add item"
+          />
         </button>
       </div>
     </article>
