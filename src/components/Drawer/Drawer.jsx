bbc8d852e3db36a288a24f9443b370c34464add1
Greg-One <sgmdoc@yandex.ru>
2021-07-11 12:47:35 +0300

    add empty cart display

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -11,39 +11,62 @@ export function Drawer({ items = [], onClose, onCartRemove }) {
             <img src="/img/remove.svg" alt="Remove" />
           </button>
         </div>
-        <div className={styles.items}>
-          {items.map((item, index) => {
-            return (
-              <div className={styles.item} key={index}>
-                <img width={70} height={70} src={item.image} alt="Sneakers" />
-                <div className={styles.info}>
-                  <p>{item.title}</p>
-                  <b>{item.price} руб.</b>
-                </div>
-                <button onClick={() => onCartRemove(item.id)}>
-                  <img src="/img/remove.svg" alt="Remove" />
-                </button>
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.block}>
-          <ul>
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 руб.</b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
-          <button className={styles.buttonGreen}>
-            Оформить заказ <img src="/img/cart-arrow.svg" alt="Arrow" />
-          </button>
-        </div>
+
+        {items.length > 0 ? (
+          <>
+            <div className={styles.items}>
+              {items.map((item, index) => {
+                return (
+                  <div className={styles.item} key={index}>
+                    <img
+                      width={70}
+                      height={70}
+                      src={item.image}
+                      alt="Sneakers"
+                    />
+                    <div className={styles.info}>
+                      <p>{item.title}</p>
+                      <b>{item.price} руб.</b>
+                    </div>
+                    <button onClick={() => onCartRemove(item.id)}>
+                      <img src="/img/remove.svg" alt="Remove" />
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className={styles.block}>
+              <ul>
+                <li>
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>21 498 руб.</b>
+                </li>
+                <li>
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b>1074 руб.</b>
+                </li>
+              </ul>
+              <button className={styles.buttonGreen}>
+                Оформить заказ{' '}
+                <img src="/img/cart-forward-arrow.svg" alt="Arrow" />
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className={styles.empty}>
+            <img src="/img/cart-empty.jpg" alt="Empty cart" />
+            <h3 className={styles.emptyTitle}>Корзина пустая</h3>
+            <p className={styles.emptySubtitle}>
+              Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.
+            </p>
+            <button className={styles.buttonGreen}>
+              <img src="/img/cart-back-arrow.svg" alt="Arrow" /> Вернуться назад
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
